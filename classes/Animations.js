0663fb9415353961e17fd41973121f4a9e39268d
damian.przygodzki <damian.przygodzki@jazzy.pro>
2017-11-26 19:21:04 +0100

    Animation support (fix)

--- classes/Animations.js
+++ classes/Animations.js
@@ -23,17 +23,14 @@ module.exports = class Animations {
     }
     
     static christmas(light) {
-        while(true){
-            const red = Color("#ff003d");
-            const green = Color("#00ab4c");
-                
-            for(var i = 0; i < 2; i++) {
-                for(var j = 0; j < light.length; j++) {
-                    light.setPixel(j, (j + i) % 2 ? red : green);
-                }
-                
-                Utils.sleep(350);
+        const red = Color("#ff003d");
+        const green = Color("#00ab4c");
+            
+        for(var i = 0; i < 2; i++) {
+            for(var j = 0; j < light.length; j++) {
+                light.setPixel(j, (j + i) % 2 ? red : green);
             }
+            
+            Utils.sleep(350);
         }
-    }
 }
