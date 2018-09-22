c30aa10f2cd5e2aef48a217856edec2f5078227a
damian.przygodzki <damian.przygodzki@jazzy.pro>
2018-09-22 21:28:04 +0200

    Animation as pseudo-code (fix #1)

--- classes/Animation.js
+++ classes/Animation.js
@@ -1,14 +1,16 @@
 'use strict';
 
+const Color = require('color');
+
 module.exports = class Animation {
     constructor (light, script, frameDuration) {
         this.frame = 0;
         this.frameDuration = frameDuration;
         this.light = light;
 
-        this.frameAction = function(frameNo, lightLength, setPixel, getPixel){
+        this.frameAction = function(frameNo, lightLength, setPixel, getPixel, Color){
             eval(script);
-        }.bind(this, this.frame, this.light.length, this.light.setPixel, this.light.getPixel);
+        }.bind(this, this.frame, this.light.length, this.light.setPixel, this.light.getPixel, Color);
     }
 
     stop () {
@@ -19,7 +21,7 @@ module.exports = class Animation {
         this.frame = 0;
 
         this.loop = setInterval(() => {
-            this.frameAction(this.frame, this.light);
+            this.frameAction();
             this.frame++;
         }, this.frameDuration);
     }
